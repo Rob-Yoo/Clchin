0305b399538fb67cfc9c077b9bc686b454e7e280
Rob-Yoo <jin12243@naver.com>
2024-08-14 21:54:12 +0900

    Chore: 프로젝트 환경 설정

---
+++ Clchin/Clchin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clchin
+//
+//  Created by Jinyoung Yoo on 8/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
